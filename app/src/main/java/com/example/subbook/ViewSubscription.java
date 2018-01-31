682802435d5dcb96fff0d689da82d7b70f6540e4
Nicholas Whitefield <Nick@Nicholass-MBP.hitronhub.home>
2018-01-30 23:16:01 -0700

    proper names appear in both main activity and view_subscription activity

--- app/src/main/java/com/example/subbook/ViewSubscription.java
+++ app/src/main/java/com/example/subbook/ViewSubscription.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ViewSubscription extends AppCompatActivity {
 
@@ -14,8 +16,16 @@ public class ViewSubscription extends AppCompatActivity {
 
         // To retrieve object in second Activity
         Subscription selectedItem = (Subscription) getIntent().getSerializableExtra("Selected_Subscription");
+        String nameText = (String) selectedItem.getName();
 
-        Log.d("OBJECT_PASSED", "Subscription passed: " + selectedItem.getName());
+        //bodyText = (EditText) findViewById(R.id.body);
+
+        //EditText subText = (EditText) view_subscription.findViewById(R.id.subNameRow);
+        EditText subText = (EditText) findViewById(R.id.subscriptionViewName);
+
+        subText.setText(nameText, TextView.BufferType.NORMAL);
+
+        //Log.d("OBJECT_PASSED", "Subscription passed: " + selectedItem.getName());
 
 
     }
